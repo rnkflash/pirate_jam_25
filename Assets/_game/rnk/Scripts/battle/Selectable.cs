4be648adefce7294640224b5149722bf5333e772
timur.nikolaev <yasnovadishu@gmail.com>
2025-01-31 16:07:18 +0500

    upd enemy view

--- Assets/_game/rnk/Scripts/battle/Selectable.cs
+++ Assets/_game/rnk/Scripts/battle/Selectable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _game.rnk.Scripts.battleSystem
@@ -12,6 +13,26 @@ namespace _game.rnk.Scripts.battleSystem
         {
             this.canBeSelected = canBeSelected;
             selection.SetActive(canBeSelected);
+        }
+        
+        private RectTransform uiElement;
+        public float minScale = 0.9f;
+        public float maxScale = 1.1f;
+        public float duration = 1f;
+
+        private void Start()
+        {
+            uiElement = selection.GetComponent<RectTransform>();
+            StartLoopingScale();
+        }
+
+        private void StartLoopingScale()
+        {
+            uiElement.DOScale(minScale, duration)
+                .SetEase(Ease.InOutSine)
+                .OnComplete(() => uiElement.DOScale(maxScale, duration)
+                    .SetEase(Ease.InOutSine)
+                    .OnComplete(StartLoopingScale));
         }
     }
 }
