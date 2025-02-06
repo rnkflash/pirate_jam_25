09151b6273a2bc133291c015922f4f156b38a463
timur.nikolaev <yasnovadishu@gmail.com>
2025-02-06 21:17:35 +0500

    add torch sound

--- Assets/_game/tinypack/data/sounds/Sound_AmbienceForest.cs
+++ Assets/_game/tinypack/data/sounds/Sound_AmbienceForest.cs
@@ -6,6 +6,6 @@ public class Sound_AmbienceForest : CMSEntity
     public Sound_AmbienceForest()
     {
         Define<AmbientTag>().clip = "audio/dungeon_phonk".Load<AudioClip>();
-        Define<AmbientTag>().volume = 0.5f;
+        Define<AmbientTag>().volume = 0.0f;
     }
 }
