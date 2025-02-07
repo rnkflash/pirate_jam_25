795911d67be569b2e3815896b596fdd16b0ac657
StepanMaksimov <stepan.maximov@mytona.com>
2025-02-07 22:04:05 +0700

    increased physics emulation speed

---
+++ Assets/_game/rnk/Scripts/battle/dice/PhysicsSimulation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _game.rnk.Scripts.battle.dice
+{
+    public class PhysicsSimulation : MonoBehaviour
+    {
+        [SerializeField] float physicsTimeStep = 0.02f;
+        [SerializeField] float physicsSpeed = 1.0f;
+        
+        float timer;
+
+        void Update()
+        {
+            if (Physics.simulationMode != SimulationMode.Script)
+                return;
+
+            timer += Time.deltaTime;
+
+            while (timer >= physicsTimeStep)
+            {
+                timer -= physicsTimeStep;
+                Physics.Simulate(physicsTimeStep * physicsSpeed);
+            }
+        }
+    }
+}
